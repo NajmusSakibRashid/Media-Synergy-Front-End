489f6097020d441a4a98ff400f8e8f6ba4f2beba
suvro <suvro@suvros-MacBook-Air.local>
2024-01-26 23:06:15 +0600

    components added for homepage- part1

--- app/user/page.jsx
+++ app/user/page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import User from '@/app/components/user'
 import LeftInfo from '@/app/components/left-info'
 import UpComingContents from '@/app/components/upcoming-contents'
+import CardButton from '@/app/components/card-button'
 
 export default function userPage() {
   return (
@@ -11,7 +12,23 @@ export default function userPage() {
         <LeftInfo/>
       </div>
       <div className='basis-8/12 p-4 flex-grow-0 flex-shrink-0'>
-        Hello World
+        <div className='grid grid-cols-3 gap-4'>
+          <CardButton
+            title='Build Profile'
+            img='https://static.vecteezy.com/system/resources/previews/010/336/740/original/construction-icon-isolated-on-transparent-background-png.png'
+            link='/src/pages/build-profile.html'
+          />
+          <CardButton
+            title='Create Content'
+            img='https://cdni.iconscout.com/illustration/premium/thumb/content-creator-ideate-the-content-8628582-6873876.png?f=webp'
+            link='/src/pages/build-profile.html'
+          />
+          <CardButton
+            title='Community Suggestions'
+            img='https://cdn.pixabay.com/photo/2016/10/18/18/59/community-1751058_640.png'
+            link='/src/pages/build-profile.html'
+          />
+        </div>
       </div>
       <div className='basis-2/12 p-4 flex-grow-0 flex-shrink-0'>
         <UpComingContents/>
