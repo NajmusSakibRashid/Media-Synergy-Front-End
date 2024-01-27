2468904bdb3e8816f3d78f7b2c152280d885ac79
suvro <suvro@suvros-MacBook-Air.local>
2024-01-27 17:50:55 +0600

    profile-cards done

---
+++ app/components/profile-cards.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function profileCards({
+  profileTitle,
+  profileImg,
+  profileDescription,
+  profileLink,
+}) {
+  return (
+    <div class="card w-full bg-base-100 shadow-xl h-full">
+      <figure className="px-10 pt-10">
+        <img src={profileImg} />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{profileTitle}</h2>
+        <p>{profileDescription}</p>
+        <div className="card-actions">
+          <button
+            className="btn btn-primary"
+            onClick={() => (window.location.href = profileLink)}
+          >
+            Visit Profile
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
