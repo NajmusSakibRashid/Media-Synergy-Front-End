5bc3ad9da7b08db9e88d240911e56f561a5e545b
SuvroGfz <1905020@ugrad.cse.buet.ac.bd>
2024-02-10 01:45:52 +0600

    finally done analytics

--- app/content/page.jsx
+++ app/content/page.jsx
@@ -11,6 +11,7 @@ import ContentTags from "../components/contents/content-tags";
 import ContentScheduleButton from "../components/contents/content-schedule-button";
 import ContentSectionHeader from "../components/contents/content-section-header";
 import BarChart from "../components/contents/analytics-bar-chart";
+import AnalyticsTripleReach from "../components/contents/analytics-tripple-stats";
 
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
@@ -157,64 +158,16 @@ export default function profilePage() {
           },
         ]}
       />  */}
-            <div className="stats rounded-none border-solid">
-              <div className="stat">
-                <div className="stat-figure text-primary">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block w-8 h-8 stroke-current"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    ></path>
-                  </svg>
-                </div>
-                <div className="stat-title">Total Likes</div>
-                <div className="stat-value text-primary">25.6K</div>
-                <div className="stat-desc">21% more than last month</div>
-              </div>
-              <div className="stat">
-                <div className="stat-figure text-secondary">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block w-8 h-8 stroke-current"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M13 10V3L4 14h7v7l9-11h-7z"
-                    ></path>
-                  </svg>
-                </div>
-                <div className="stat-title">Post Views</div>
-                <div className="stat-value text-secondary">2.6M</div>
-                <div className="stat-desc">21% more than last month</div>
-              </div>
-
-              <div className="stat">
-                <div className="stat-figure text-secondary">
-                  <div className="avatar online">
-                    <div className="w-16 rounded-full">
-                      <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                    </div>
-                  </div>
-                </div>
-                <div className="stat-value">86%</div>
-                <div className="stat-title">Liked and Followed</div>
-                <div className="stat-desc text-secondary">
-                  31 tasks remaining
-                </div>
-              </div>
-            </div>
+           
             <div className="card-title self-center">Reach</div>
+
+            <AnalyticsTripleReach
+              reactionCount="100"
+              postViews="2.6M"
+              fanViews="86"
+              storyViews="14"
+            />
+
             <canvas id="lineChart"></canvas>
             {/* <div className="self-center">
               <p className="text-xl">
