a843746099cdeeaf14a66ab8166bc72d3f9bf363
NajmusSakibRashid <55847267+NajmusSakibRashid@users.noreply.github.com>
2024-06-23 00:21:11 +0600

    history deleted

--- .history/app/user/contents/analytics/[platform]/[postId]/page_20240302235351.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import facebook from '@/app/components/contents/facebook'
-
-export default function page(params) {
-  const renderItem={
-    facebook:<facebook>{params.postId}</facebook>
-  }
-  return renderItem[params.platform]
-}
